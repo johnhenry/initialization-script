f66f7c895979358177ff66db60a4be730942b0ac
John <john@iamjohnhenry.com>
2016-11-13 12:36:56 -0800

    Add transform-async-generator-functions to node-next.sh

--- .scripts/node-next.sh
+++ .scripts/node-next.sh
@@ -3,6 +3,7 @@ npm install --save-dev \
 babel-cli \
 babel-polyfill \
 babel-plugin-transform-es2015-destructuring \
+babel-plugin-transform-async-generator-functions\
 babel-preset-stage-3 \
 babel-preset-latest \
 babel-preset-react \
@@ -12,4 +13,4 @@ mkdir node
 echo 'console.log("hi");' >> node/index.js
 
 #create scripts
-npm run set scripts node 'babel-node --debug --presets latest,react,stage-3 --plugins transform-es2015-destructuring node/index.js'
+npm run set scripts node 'babel-node --debug --presets latest,react,stage-3 --plugins transform-es2015-destructuring,transform-async-generator-functions node/index.js'
